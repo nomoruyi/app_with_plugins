c056ccf1d07c0bc73bb039313d013089de515eb4
Nosa <no@winkler-software.io>
2024-07-03 11:16:40 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_with_plugins/app/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/plugins/p1/plugin_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Plugin1 extends StatefulWidget {
+  const Plugin1({super.key});
+
+  @override
+  State<Plugin1> createState() => _Plugin1State();
+}
+
+class _Plugin1State extends State<Plugin1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
